53e7da6504f5e0492d651aea89b49024970a84c3
Macbook Air <air@MacbookdeMacBook-Air.local>
2016-10-13 15:32:40 +0800

    完成login.html

---
+++ opsmanager/views.py
@@ -0,0 +1,9 @@
+from  django.shortcuts import  render
+from django.http import  HttpResponse
+
+
+def login(request):
+    return  render(request,'login.html')
+
+def index(request):
+    return  HttpResponse('test')
