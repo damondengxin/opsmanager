1aa38ae49943f9c835358285eec26fce2a3d1562
Macbook Air <air@MacbookdeMacBook-Air.local>
2016-10-14 15:46:07 +0800

    完成login并能登录,检测错误

--- opsmanager/views.py
+++ opsmanager/views.py
@@ -1,9 +1,14 @@
 from  django.shortcuts import  render
-from django.http import  HttpResponse
+from django.http import  HttpResponse,HttpResponseRedirect
+from django.contrib.auth.decorators import  login_required
+from django.core.urlresolvers import reverse
 
 
-def login(request):
-    return  render(request,'login.html')
+
 
 def index(request):
-    return  HttpResponse('test')
+    if not request.user.is_authenticated():
+        return HttpResponseRedirect(reverse('login'))
+    else:
+        #return HttpResponseRedirect(reverse('servers_list'))
+        return  HttpResponse('test')
